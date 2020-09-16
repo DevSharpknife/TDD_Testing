865e4da5f454b78808204a06d52a46d6137ee779
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-16 23:49:52 +0100

    Removed original test as it was pointless and refactored the compare function

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -4,5 +4,8 @@ from src.compare import compare
 
 class TestCompare(unittest.TestCase):
 
-    def test_compare_3_1_returns_3_is_greater_than_1(self):
+    # def test_compare_3_1_returns_3_is_greater_than_1(self):
+    #     self.assertEqual("3 is greater than 1", compare(3, 1))
+    
+    def test_compare__greater_than(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
